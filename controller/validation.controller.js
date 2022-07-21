5cf625891250bcb6d15411004dca291d01d9ba22
anjumariah <98024806+anjumariah@users.noreply.github.com>
2022-07-21 15:50:14 +0530

    booking

--- controller/validation.controller.js
+++ controller/validation.controller.js
@@ -1,38 +1,62 @@
 const jwt = require("jsonwebtoken");
-const authModel = require('../models/validation')
-const bcrypt = require('bcrypt')
-const saltRounds = 10
-// console.log(authModel,"model ########");
+const authModel = require('../models/validation.models')
+// const bcrypt = require('bcrypt')
+// const saltRounds = 10
+// console.log(authModel,"model SIGNUP ########");
 
 async function signup(req,res){
-    
-   console.log("controller");
-   console.log(req.body," sign up in controller"); 
-
-   const name= req.body.name;
-   const email = req.body.email;
-   const password = req.body.password; 
-   authModel.signup(name,email,password);
+    try{
+    console.log("controller SIGNUP");
+    console.log(req.body," sign up "); 
+ 
+    const name= req.body.name;
+    const email = req.body.email;
+    const password = req.body.password; 
+    authModel.signup(name,email,password);
+    return res.status(200).json({"sucess": true, message: 'hi'})
+   }catch(err){
+      return res.json({message:"error"}).status(500);
+   }
+   
    
 }
 
 async function login(req,res){
-   console.log(req.body)
-   console.log(req.body," login in controller"); 
-   const email = req.body.email;
-   const password = req.body.password; 
-  const loginRes = await authModel.login(email,password);
-  console.log(loginRes,"---Logresonse---");
-
-  if(loginRes.length>0){
-   let token =jwt.sign({id:loginRes.id,email:loginRes.email},"secrettoken",{expiresIn:600})
-   console.log(token,"creating token in login in controller");
-   return res.json({ success: true,token }).status(200);
+   try{
+      // console.log(req.body)
+      // console.log(req.body," login in controller"); 
+      const email = req.body.email;
+      const password = req.body.password; 
+     const loginRes = await authModel.login(email,password);
+   //   console.log(loginRes,"---Logresonse---");
+   
+     if(loginRes.length>0){
+      let token = jwt.sign({id:loginRes.id,email:loginRes.email},"secrettoken",{expiresIn:'10h'})
+      // console.log(token,"creating token in login in controller");
+      return res.json({userEmail:email,usePassword:password, success: true,token }).status(200);
       
-  }else{
-   res.status(400).send({error:"token not provided"})
+     }else{
+      res.status(400).send({success:false,error:"token not provided"})
+     }
+   }catch(err){
+      res.status(400).send({success:false,error:"token creation failed"})
+     
+     }
+
+}
+
+module.exports={
+   signup,
+   login
   
-  }
+};
+
+
+
+
+
+
+
   
    // if (!req.body.name && !req.body.password) {
    //    return res.status(500).send({
@@ -42,63 +66,35 @@ async function login(req,res){
    //    return res.send({email,password})
    // }
    
-   // console.log(req.token)
-   // console.log(req.token," login in controller"); 
-   // const{token}=req.body
-   // jwt.verify(token,"secrettoken",function(err,decoded){
-   //    if(err){
 
-   //    }
-   // })
+
+// async function verifyToken(req,res,next){
    
-}
-// const createToken =  (req,res)=>{
-   // const {email, password} = req.body;
-   // const signableToken = email + password;
-   // const logVerify= login(email,password)
-   
+//    const {email, password} = req.body;
+//    const signableToken = email + password;
+//    console.log(signableToken,"--------signable token in rq.body---");
 
-   //   let token = jwt.sign(logVerify,"secrettoken",{expiresIn:600})
-   //   console.log(signableToken,"-------signabletoken------");
-   //  console.log(token,"jwt sign in validate.controler");
-   // return res.json({auth:true,token:token})
-   
-   
-//  }
+//    // const authHeader =req.headers["authorization"];
+//    // if(authHeader ===undefined){
+//    //    res.send({error:"token not created"})
+//    // }
+//    // let token = authHeader.split(" ")[1]
 
-
-function verifyToken(req,res,next){
-   
-   const {email, password} = req.body;
-   const signableToken = email + password;
-   console.log(signableToken,"--------signable token in rq.body---");
-
-   // const authHeader =req.headers["authorization"];
-   // if(authHeader ===undefined){
-   //    res.send({error:"token not created"})
-   // }
-   // let token = authHeader.split(" ")[1]
-
-   jwt.verify(signableToken,"secrettoken",function(err,decoded){
-      if(err){
-         res.send({error:"authentication failed"})
+//    jwt.verify(signableToken,"secrettoken",function(err,decoded){
+//       if(err){
+//          res.send({error:"authentication failed"})
       
-      }else{
-         // req.userId = decoded.id
-         res.json({token:signableToken})
-            console.log(signableToken,",^^^^^^^^^^^^^^^^^^^^^^^");
-            console.log(token,"%%%%%%%%%%%%");
-         next()
-      }
-   })
-}
+//       }else{
+//          // req.userId = decoded.id
+//          res.json({token:signableToken})
+//             console.log(signableToken,",^^^^^^^^^^^^^^^^^^^^^^^");
+//             console.log(token,"%%%%%%%%%%%%");
+//          next()
+//       }
+//    })
+// }
 
-module.exports={
-   signup,
-   login,
-   verifyToken
-  
-};
+
 
 
 // async function validateLogin(req,res){
--- routes/auth.js
+++ routes/auth.js
@@ -1,26 +1,22 @@
 const  express = require("express");
-const {signup,login, verifyToken} = require("../controller/validation.controller");
+const {signup,login} = require("../controller/validation.controller");
+const {verifyToken} = require("../middlewear/verifyToken")
+
 
 const router = express.Router()
 
 
-router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  router.use(express.json());
-// router.get('/',(req,res) => {
-//     console.log("hi");
-//     res.send("anju")
-// })
-router.post('/signup',signup)
+router.post('/signup',signup);
 
-router.post('/login',login)
+router.post('/login',login);
+
+
 
 router.get('/an',(req,res) => {
     console.log("hi");
-    res.send("ajay")
+    res.send("ajay");
+    res.send(req.body)
 })
 
 module.exports = router;
+
