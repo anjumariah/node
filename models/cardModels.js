960a0e7a176cd2bf22b7336828cb2a49812e6850
anjumariah <98024806+anjumariah@users.noreply.github.com>
2022-07-22 10:39:47 +0530

    second

---
+++ models/cardModels.js
@@ -0,0 +1,21 @@
+const makeDbconfig = require('../config/db.config')
+
+async function card(){
+    const db = makeDbconfig.makeDb();
+    // console.log("-------model-----------------");
+    try{
+        const sql ="SELECT * FROM card ";
+        const sqlCard= await db.query(sql)
+        // console.log(sqlCard,"---CARDS------");
+        return sqlCard;
+    }catch{
+        console.log(err);
+        return false
+    }finally{ 
+        await db.close();
+      }
+}
+
+module.exports = {
+    card
+}
